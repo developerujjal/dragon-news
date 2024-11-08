c5837ff1a906992a6cd29bd8cb795c5f8c145e62
Developer Ujjal <ujjalchandra213@gmail.com>
2024-11-09 03:28:16 +0600

    added data

--- src/layouts/SharedLayout/Header.jsx
+++ src/layouts/SharedLayout/Header.jsx
@@ -1,6 +1,8 @@
 import logo from '../../assets/logo.png'
+import moment from 'moment';
 
 const Header = () => {
+
     return (
         <div className="text-center pt-8 pb-6 md:px-72">
             <div className='mb-3.5'>
@@ -8,10 +10,12 @@ const Header = () => {
             </div>
             <div>
                 <p className='text-[#DADADA] font-normal mb-1 text-base'><span>Journalism Without Fear or Favour</span></p>
-                <p className='text-[#706F6F] text-[17px]'><span><span className='text-[#403F3F]'>Sunday,</span> November 27, 2025</span></p>
+                <p className='text-[#706F6F] text-[17px]'><span>{moment().format("dddd, MMMM D, YYYY")}</span></p>
             </div>
         </div>
     );
 };
 
 export default Header;
+
+// text-[#403F3F]
